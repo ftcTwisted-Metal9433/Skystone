a86b46f8717075f1b7fb2e4a88829704dfefc91c
OliverLavin <oliverwlavin@icloud.com>
2019-11-21 17:24:46 -0800

    Create Test_encoders.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_encoders.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous (name= "Test_encoder")
+public class Test_encoders extends LinearOpMode {
+
+    DcMotor frontRight;
+    DcMotor frontLeft;
+    DcMotor backRight;
+    DcMotor backLeft;
+    DcMotor pullyBoi;
+    Servo servoboiRight;
+    Servo servoboiLeft;
+
+    public void code(){
+
+    }
+    @Override
+    public void runOpMode() throws InterruptedException
+        fro
+
+
+            throws InterruptedException {
+
+    }
+}
